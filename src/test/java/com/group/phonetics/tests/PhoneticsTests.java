bdf9ae940a05a4de12c5d02cdeafd2c5b1958aa1
Marcos Rachid <marcosrachid@gmail.com>
2015-12-06 14:28:06 -0200

    junit tests

---
+++ src/test/java/com/group/phonetics/tests/PhoneticsTests.java
@@ -0,0 +1,109 @@
+package com.group.phonetics.tests;
+
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
+
+import org.junit.Ignore;
+import org.junit.Test;
+
+import com.group.phonetics.pojo.Phonetics;
+import com.group.phonetics.util.Helper;
+
+public class PhoneticsTests {
+
+	@Ignore
+	public void emptyTest(){
+		
+		String args[] = new String[]{};
+		
+		Phonetics po = new Phonetics();
+		try {
+			po.setEnviroment(args);
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+	
+	@Ignore
+	public void noWordTest(){
+		
+		String args[] = new String[]{"<","C:\\Users\\marcosrachid\\workspace\\SearchPhonetics\\src\\main\\resource\\word_dict.txt"};
+		
+		Phonetics po = new Phonetics();
+		try {
+			po.setEnviroment(args);
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+	
+	@Ignore
+	public void noDelimiterTest(){
+		
+		String args[] = new String[]{"1ton#", "brief", "soon", "C:\\Users\\marcosrachid\\workspace\\SearchPhonetics\\src\\main\\resource\\word_dict.txt"};
+		
+		Phonetics po = new Phonetics();
+		try {
+			po.setEnviroment(args);
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+	
+	@Ignore
+	public void noInputFileTest(){
+		
+		String args[] = new String[]{"1ton#", "brief", "soon", "<"};
+		
+		Phonetics po = new Phonetics();
+		try {
+			po.setEnviroment(args);
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+	
+	@Ignore
+	public void MultipleInputFileTest(){
+		
+		String args[] = new String[]{"1ton#", "brief", "soon", "<", "C:\\Users\\marcosrachid\\workspace\\SearchPhonetics\\src\\main\\resource\\word_dict.txt", "C:\\Users\\marcosrachid\\workspace\\SearchPhonetics\\src\\main\\resource\\word_dict.txt"};
+		
+		Phonetics po = new Phonetics();
+		try {
+			po.setEnviroment(args);
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+	
+	@Ignore
+	public void inputFileNotFoundTest(){
+		
+		BufferedReader br = null;
+		try {			
+			br = new BufferedReader(new FileReader("C:\\Users\\marcosrachid\\workspace\\SearchPhonetics\\src\\main\\resource\\word_dict.tx"));
+		} catch (FileNotFoundException e) {
+			System.err.println(e.getMessage());
+		} catch (IOException e) {
+			System.err.println(e.getMessage());
+		} finally {
+			if (br != null) {
+				try {
+					br.close();
+				} catch (IOException e) {
+				}
+			}
+		}
+		
+	}
+	
+}
